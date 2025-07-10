3a9c1bca2c720a84854625a6ce9b12812f5c8de7
Hak <hieptth71@gmail.com>
2025-07-10 22:48:29 +0700

    feature: finishing touch for products section (#8)
    * fix(products): fix hovercard hover state
    
    * feat(carousel): auto hover state for card
    
    * fix(carousel): make the middle card active instead
    
    * refactor(carousel): move styles

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -5,62 +5,19 @@ import { AutoPlay } from "@egjs/flicking-plugins";
 import "@egjs/flicking-plugins/dist/flicking-plugins.css";
 import Flicking from "@egjs/react-flicking";
 import "@egjs/react-flicking/dist/flicking.css";
-
-const flickers = [
-  {
-    img: "/assets/keyboard.png",
-    title: "Mechanical Keyboard",
-    description: "High-quality mechanical keyboard with customizable keys.",
-  },
-  {
-    img: "/assets/keyboard.png",
-    title: "Gaming Mouse",
-    description: "Ergonomic gaming mouse with RGB lighting.",
-  },
-  {
-    img: "/assets/keyboard.png",
-    title: "Wireless Headset",
-    description: "Noise-cancelling wireless headset with long battery life.",
-  },
-  {
-    img: "/assets/keyboard.png",
-    title: "4K Monitor",
-    description: "Ultra HD monitor with vibrant colors and high refresh rate.",
-  },
-  {
-    img: "/assets/keyboard.png",
-    title: "Gaming Laptop",
-    description: "Powerful gaming laptop with high-end graphics card.",
-  },
-  {
-    img: "/assets/keyboard.png",
-    title: "4K Monitor",
-    description: "Ultra HD monitor with vibrant colors and high refresh rate.",
-  },
-  {
-    img: "/assets/keyboard.png",
-    title: "Gaming Laptop",
-    description: "Powerful gaming laptop with high-end graphics card.",
-  },
-  {
-    img: "/assets/keyboard.png",
-    title: "4K Monitor",
-    description: "Ultra HD monitor with vibrant colors and high refresh rate.",
-  },
-  {
-    img: "/assets/keyboard.png",
-    title: "Gaming Laptop",
-    description: "Powerful gaming laptop with high-end graphics card.",
-  },
-];
+import { useState } from "react";
+import { CARD_ITEMS } from "./constants";
 
 const Products = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
   const plugins = [
     new AutoPlay({ duration: 3000, direction: "NEXT", stopOnHover: true }),
   ];
 
   return (
-    <section id="products" className="flex flex-col items-center gap-16">
+    <section id="products" className="flex flex-col items-center gap-16 pb-32">
       <div className="flex flex-col gap-4 text-center px-4 lg:max-w-2/3">
         <p className="text-3xl/7.5 font-medium lg:text-5xl/15 uppercase text-[#0C0C0C] tracking-tight">
           Popular Products
@@ -73,22 +30,35 @@ const Products = () => {
       </div>
 
       <Flicking
-        align="prev"
+        align="center"
         circular={true}
         circularFallback="bound"
         deceleration={0.005}
         duration={1000}
+        adaptive={true}
         plugins={plugins}
         hideBeforeInit={true}
         className="w-full"
+        style={{ overflow: "unset", height: "395px !important" }}
+        onWillChange={(e) => setActiveIndex(e.index)}
       >
-        {flickers.map((item, index) => (
-          <div key={index} className="px-5">
+        {CARD_ITEMS.map((item, index) => (
+          <div
+            key={index}
+            className="flex items-end box-border"
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
             <HoverCard
               props={{
+                index,
                 img: item.img,
                 title: item.title,
                 description: item.description,
+                isActive:
+                  hoveredIndex === null
+                    ? index === activeIndex
+                    : index === hoveredIndex,
               }}
             />
           </div>
